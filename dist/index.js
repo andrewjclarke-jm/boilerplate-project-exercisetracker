fe584fb7b5c9c7b82addcfa96f32a3b487484612
Andrew Clarke <aclarke@griddynamics@gmail.com>
2023-11-19 15:38:52 -0500

    start basic server

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,11 @@
-"use strict";
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const expressApplicationInstance = express();
+const applicationPortNumber = process.env.APPLICATION_PORT_NUMBER || 3000;
+const applicationStartCallbackHandler = () => { };
+expressApplicationInstance.get("/", (req, res) => res.send("connected"));
+const startExpressApplicationServer = (portNumber, callback) => {
+    expressApplicationInstance.listen(portNumber, callback);
+};
+startExpressApplicationServer(applicationPortNumber, applicationStartCallbackHandler);
--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
+const expressApplicationInstance = express();
+const applicationPortNumber = process.env.APPLICATION_PORT_NUMBER || 3000;
+const applicationStartCallbackHandler = () => {};
+
+expressApplicationInstance.get("/", (req: Request, res: Response) =>
+  res.send("connected")
+);
+
+const startExpressApplicationServer = (
+  portNumber: number | string,
+  callback: () => void
+) => {
+  expressApplicationInstance.listen(portNumber, callback);
+};
+
+startExpressApplicationServer(
+  applicationPortNumber,
+  applicationStartCallbackHandler
+);
